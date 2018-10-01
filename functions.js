46402916b5cf4ed7d01da7976c80dfb496ec147e
SHULL <justinshull2@gmail.com>
2018-10-01 09:27:25 -0500

    added order function

--- functions.js
+++ functions.js
@@ -3,11 +3,20 @@ window.addEventListener("load", pageSetUp);
 var pizzas = ["Cheese", 5.00 ,"Pepperoni", 6.00 , "Meat Lovers", 8.00 , "Vegetarian", 7.00 , "Buffalo Chicken", 10.00]
 
 function pageSetUp(){
-    for (i=0; i<10; i++){
+    for (i=1; i<11; i++){
     document.getElementById("1-10").innerHTML += `<option value="${i}">${i}</option>`;
     }
     for (j=0; j<pizzas.length - 1; j++){
         document.getElementById("Type").innerHTML += `<option value="${pizzas[j]}">${pizzas[j]}</option>`;
         j++;
     }
+}
+
+function Order(){
+    var name = document.getElementById("Name").value;
+    var phone = document.getElementById("Phone").value;
+    var numPizza = document.getElementById("1-10").value;
+    var pizzaType = document.getElementById("Type").value;
+
+    document.getElementById("OrderConf").innerHTML = `${name}</br>${phone}</br>${numPizza}</br>${pizzaType}`;
 }
