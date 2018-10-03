10dc3b77d62e349650fbc4174d507aa936e7f601
SHULL <justinshull2@gmail.com>
2018-10-03 08:05:55 -0500

    removed unnecessary semicolon

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ var pizzas = ["Cheese","Pepperoni", "Meat Lovers", "Vegetarian", "Buffalo Chicke
 function pageSetUp(){
     for (i=1; i<11; i++){
     document.getElementById("1-10").innerHTML += `<option value="${i}">${i}</option>`;
-    };
+    }
     for (j=0; j<pizzas.length; j++){
         document.getElementById("Type").innerHTML += `<option value="${pizzas[j]}">${pizzas[j]}</option>`;
     }
